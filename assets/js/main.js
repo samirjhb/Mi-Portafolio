7ba928b7148563f60376577fe4a6cb10a9c9facc
samir  jose hadechni bonett <samirjhb@MBP-de-samir.lan>
2022-09-18 21:51:10 -0300

    Avance v0.1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,49 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== services ===============*/
+
+const modalViews=document.querySelectorAll('.services_modal'),
+modalBtns= document.querySelectorAll('.services_button'),
+modalClose= document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i)=>{
+    mb.addEventListener( 'click', ()=>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click', ()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+
+})
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
